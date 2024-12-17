358715808dbdb3c6b09363972c0f2f832d86b532
Maiju19 <maiju.hakulinen@myy.haaga-helia.fi>
2024-12-17 13:02:20 +0200

    Pieniä muutoksia

---
+++ src/Etusivu.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./Etusivu.css";
+
+
+
+function Etusivu() {
+
+  
+  return (
+    <div>
+      <h1>Olet kirjautunut sisään!</h1>
+      <p>Täällä voit tutustua taideteoksiin.</p>
+    </div>
+     
+  );
+}
---
+++ src/LoginForm.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import "./App.css";
+
+function LoginForm()  {
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      setError("Kirjautuminen epäonnistui. Tarkista sähköposti ja salasana");
+      console.error("Kirjautminen epäonnistui", error);
+      setTimeout(() => setError(""), 5000);
+    }
+    
+  };
+
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Sähköposti:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Salasana:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Kirjaudu sisään</button>
+      </form>
+      {error && <p>{error}</p>} {/* Virheilmoituksen näyttäminen */}
+    </div>
+  );
+}
+
+export default LoginForm;
